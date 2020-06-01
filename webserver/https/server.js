0a7a47bf5da3cd5a827c6faeadd2eae7a0201ea4
zqj <582132116@qq.com>
2020-06-02 07:04:58 +0800

    feat: socket.io的功能
    添加socket.io组件/添加日志组件

--- webserver/https/server.js
+++ webserver/https/server.js
@@ -1,24 +1,75 @@
 'use strict'
 
 var express = require('express');
-var app = express();
+var serveIndex= require('serve-index');
 var http = require('http');
 var https = require('https');
 var fs = require('fs');
+
+var socketIo = require('socket.io');
+var log4js = require('log4js');
+
+log4js.configure({
+    appenders: {
+        file: {
+            type: 'file',
+            filename: 'app.log',
+            layout: {
+                type: 'pattern',
+                pattern: '%r %p - %m',
+            }
+        }
+    },
+    categories: {
+        default: {
+            appenders: ['file'],
+            level: 'debug'
+        }
+    }
+});
+var logger = log4js.getLogger();
+
+var app = express();
+app.use('/', express.static('./public'));
 
 var options = {
     key: fs.readFileSync('./cert/privatekey.pem'),
     cert: fs.readFileSync('./cert/certificate.pem')
 }
 
-app.use('/', express.static('./public'));
-
 var httpServer = http.createServer(app).listen(8887, function() {
         console.log('HTTP Server is running on: http://localhost:%s', 8887);
     });
 
-var httpsServer = https.createServer(options, app)
-    .listen(8888, function() {
+var httpsServer = https.createServer(options, app);
+var io = socketIo.listen(httpsServer);
+
+io.sockets.on('connection', (socket) => {
+    socket.on('join',(room)=>{
+        socket.join(room);
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = Object.keys(myRoom.sockets).length;//拿到房间里所有的人数
+        logger.log('the number of user in room is:' + users);
+
+        //socket.emit('joined', room, socket.id); //给本人回消息
+        //socket.to(room).emit('joined', root, socket.id); //除自己之外
+        //io.in(room).emit('joined', room, socket.id)//房间内所有人
+        socket.broadcast.emit('joined', room, socket.id); //除自己外，全部站点
+    });
+    socket.on('leave',(room)=>{
+        var myRoom = io.sockets.adapter.rooms[room];
+        var users = Object.keys(myRoom.sockets).length;//拿到房间里所有的人数
+        //users - 1;
+
+        socket.leave(room);
+        //socket.emit('joined', room, socket.id); //给本人回消息
+        //socket.to(room).emit('joined', root, socket.id); //除自己之外
+        //io.in(room).emit('joined', room, socket.id)//房间内所有人
+        socket.broadcast.emit('leave', room, socket.id); //除自己外，全部站点
+    });
+});
+
+httpsServer.listen(8888, function() {
         console.log('HTTPS Server is running on: http://localhost:%s', 8888);
     });
 
