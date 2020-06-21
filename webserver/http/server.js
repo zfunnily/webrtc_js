883a86d237ce8775f5351646fef926f2c9524e26
zqj <582132116@qq.com>
2020-06-21 14:22:30 +0800

    refactor: delete webserver/http

--- webserver/http/server.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-var http = require('http');
-
-var app = http.createServer(function (req, res) {
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Hello Wrold\n');
-
-}).listen(8888, '0.0.0.0');
-
